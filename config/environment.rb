c1afdae9586d3645a86992d8aad136108b982fbb
Дмитрий Василец <pronix.service@gmail.com>
2009-09-23 23:14:13 +0400

    disable encoding in env

--- config/environment.rb
+++ config/environment.rb
@@ -95,5 +95,5 @@ Time::DATE_FORMATS[:short_date] = "%Y-%m-%d"
 # Mime::Type.register "application/x-mobile", :mobile
 
 # Include your application configuration below
-Encoding.default_internal = 'utf-8'
-Encoding.default_external = 'utf-8'
+#Encoding.default_internal = 'utf-8'
+#Encoding.default_external = 'utf-8'
