4154eee44576bfcab8f3066eccbfda6be81be140
Дмитрий Василец <pronix.service@gmail.com>
2009-09-25 23:13:06 +0400

    fix migration for deploy

--- vendor/extensions/blog/db/migrate/20090120110819_extend_entry.rb
+++ vendor/extensions/blog/db/migrate/20090120110819_extend_entry.rb
@@ -2,6 +2,13 @@ class ExtendEntry < ActiveRecord::Migration
   def self.up
     add_column :comments, :name, :string
     add_column :comments, :uri,  :string
+    [{:author =>'admin',:title=>'main',:body=>'Главная'},
+      {:author =>'admin',:title=>'about_us',:body =>'О нас'},
+      {:author =>'admin',:title=>'contacts',:body =>'Контакты'},
+      {:author =>'admin',:title=>'links',:body=>'Ссылки'}].each do |x|
+      BlogEntry.create(x)
+    end
+
   end
 
   def self.down
